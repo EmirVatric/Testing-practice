2f96bcbd5ef95e1df40ab122d0d9bd584f9b6e03
abdellani <abdellani@gmail.com>
2019-09-20 18:56:45 +0100

    Add caeser method

--- index.js
+++ index.js
@@ -17,5 +17,65 @@ const calculator = {
   }
 };
 
+const caeser = (string, number) => {
 
-module.exports = { capitalize, stringReverse, calculator };
+  let checkCapital = (letter) => {
+    let ascii = letter.charCodeAt()
+    if (65 <= ascii && ascii <= 90) {
+      return true
+    }
+    return false
+  }
+  let isAlphabet = (letter) => {
+    let ascii = letter.charCodeAt()
+    if (65 <= ascii && ascii <= 90 || 97 <= ascii && ascii <= 122) {
+      return true
+    }
+    return false
+
+  }
+  let getLetterFromOrder = (number, isCapital) => {
+    if (isCapital) {
+      return String.fromCharCode(number + 64);
+    } else {
+      return String.fromCharCode(number + 96);
+    }
+  }
+
+  let getLetterOrder = (letter) => {
+    let ascii = letter.charCodeAt()
+    if (ascii <= 91) {
+      return ascii - 64;
+    } else {
+      return ascii - 96;
+    }
+  }
+  let rotate = (letterOrder, number) => {
+    if ((letterOrder + number) % 26 == 0) {
+      return 26;
+    }
+    return (letterOrder + number) % 26;
+  }
+  let result = []
+  let letter;
+  for (let i = 0; i < string.length; i++) {
+    letter = string.charAt(i)
+    if (!isAlphabet(letter)) {
+      result.push(letter)
+    } else {
+      let isCapital = checkCapital(letter)
+      let order = getLetterOrder(letter)
+      let newLetterOrder = rotate(order, number);
+      let newLetter = getLetterFromOrder(newLetterOrder, isCapital)
+      result.push(newLetter)
+    }
+  }
+  return result.join("")
+}
+
+module.exports = {
+  capitalize,
+  stringReverse,
+  calculator,
+  caeser
+};
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { capitalize, stringReverse, calculator } = require('./index');
+const { capitalize, stringReverse, calculator,caeser } = require('./index');
 
 test('Capitalize string', () => {
   expect(capitalize('test')).toBe('Test');
@@ -22,4 +22,22 @@ test('It multiplies two numbers', () => {
 
 test('It divides two numbers', () => {
   expect(calculator.divide(8, 2)).toBe(4);
+});
+
+test('it encrypt lowercase strings', () => {
+  expect(caeser("abc",1)).toBe("bcd");
+  expect(caeser("abc",-1)).toBe("zab");
+  expect(caeser("xyz",1)).toBe("yza");
+});
+
+test('it encrypt Uppercase strings', () => {
+  expect(caeser("ABC",1)).toBe("BCD");
+  expect(caeser("ABC",-1)).toBe("ZAB");
+  expect(caeser("XYZ",1)).toBe("YZA");
+});
+
+test('it encrypt alphabet characters and ignore symbols and numbers', () => {
+  expect(caeser("231654",2)).toBe("231654");
+  expect(caeser(")_=+*",2)).toBe(")_=+*");
+  expect(caeser("Abc123*",2)).toBe("Cde123*");
 });
